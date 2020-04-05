44872c12d705d44eb4381cc52379b8696ba821ce
Rok <rok.hecl89@gmail.com>
2020-04-05 22:19:58 +0200

    added docker and docker-compose

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthz(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "OK\n")
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	http.HandleFunc("/", healthz)
+
+	http.ListenAndServe(":3001", nil)
 }
