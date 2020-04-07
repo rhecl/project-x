ed098279e2cc5ad6cb4404ebb07ce0131d773532
Rok <rok.hecl89@gmail.com>
2020-04-07 21:31:41 +0200

    enable cors on server

--- api/main.go
+++ api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func healthz(w http.ResponseWriter, req *http.Request) {
@@ -10,7 +13,10 @@ func healthz(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", healthz)
-
-	http.ListenAndServe(":3001", nil)
+	router := mux.NewRouter()
+	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	router.HandleFunc("/healthz", healthz).Methods("GET")
+	http.ListenAndServe(":3001", handlers.CORS(headers, methods, origins)(router))
 }
