aa662c246cdcf5888265fd1739bf3615dea3ac0d
Rok <rok.hecl89@gmail.com>
2020-04-13 17:32:47 +0200

    moved main.go

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rhecl/project-x/pkg/mongo"
 )
 
 func healthz(w http.ResponseWriter, req *http.Request) {
@@ -17,6 +18,14 @@ func main() {
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
 	origins := handlers.AllowedOrigins([]string{"*"})
+
 	router.HandleFunc("/healthz", healthz).Methods("GET")
+
+	_, err := mongo.Connect()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
 	http.ListenAndServe(":3001", handlers.CORS(headers, methods, origins)(router))
 }
