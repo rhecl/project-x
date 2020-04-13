51b6d649039410a8c1a6408d24a3b3a35aac29cb
Rok <rok.hecl89@gmail.com>
2020-04-13 19:05:59 +0200

    added verification for mongo connection

--- api/pkg/mongo/connect.go
+++ api/pkg/mongo/connect.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Connect() (*mongo.Client, error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	// TODO: correctly handle connection error
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+os.Getenv("MONGO_HOST")+":"+os.Getenv("MONGO_PORT")))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://"+os.Getenv("MONGO_HOST")+":"+os.Getenv("MONGO_PORT")))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.TODO(), nil)
 
 	if err != nil {
 		return nil, err
